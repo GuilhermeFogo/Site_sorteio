d200b227a9fc6b974f9f0bfc7f23ee7037c65b90
GuilhermeFogo <gui.fogo17@gmail.com>
2018-12-17 07:44:06 -0200

    Fazendo ultimas alterações do programa

--- PHP/HelperCampos/Campos_sorteio.php
+++ PHP/HelperCampos/Campos_sorteio.php
@@ -2,25 +2,29 @@
 
 class Campos_sorteio 
 {
-	private $array_nome;
-	private $array_email;
-	private $indices;
-	function __construct($indice)
+	private $nomes;
+	private $emails;
+	private $indice;
+	function __construct($indices)
 	{
-		$this->$indices = $indice;
-		$this->$array_nome = array($_POST['sorteio_nome'.$indice]);
-		$this->$array_email = array($_POST['sorteio_email'.$indice]);
+
+		$this->$nomes =$_POST['sorteio_nome'.$indices];
+		$this->$emails =$_POST['sorteio_email'.$indices];
+		$this->$indice =$indices;
 
 	}
 
 	public function Sortear_tudo()
 	{
-		$numero_randomico= random_int(0, 50);
-		$numero_randomico =$numero_randomico * pi();
+		if ($this->$indice<=0||$this->$indice==1) {
+			echo "ERRO";
+		}else{
 
-		$arrayName = array($this->$array_nome,$this->$array_emails);
-		foreach ($arrayName as $variable) {
-			echo $variable. " ". $numero_randomico;
+			for ($i=0; $i <$this->$indice ; $i++) { 
+				echo "<br>";
+				echo $this->$indice;
+			}//for
+
 		}
 	}
 
